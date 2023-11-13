7fd2779f3f7a5ebd0510bfe02fd7931d2734c431
Junior Facin <jufacin@hotmail.com>
2023-11-13 16:32:09 -0300

    URL params concluded

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink (link) {
@@ -10,3 +11,14 @@ function ativarLink (link) {
 
 links.forEach(ativarLink) 
 
+// Ativar Itens do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if(elemento) {
+    elemento.checked = true;
+    }
+  }
+
+parametros.forEach(ativarProduto);
